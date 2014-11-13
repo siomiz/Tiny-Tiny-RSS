bf52b08b8ae596065fa327bd95709dcb13a694c4
Tomohisa Kusano <siomiz@gmail.com>
2014-11-14 08:58:01 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+FROM debian:wheezy
+
+MAINTAINER Tomohisa Kusano <siomiz@gmail.com>
+
+RUN apt-get update \
+	&& DEBIAN_FRONTEND=noninteractive \
+	apt-get -y install \
+	git-core \
+	nginx \
+	php5-cli \
+	php5-curl \
+	php5-fpm \
+	php5-gd \
+	php5-json \
+	php5-pgsql \
+	postgresql \
+	supervisor
+
+ENV TTRSS_VERSION_TAG 1.14
+
+RUN git clone https://github.com/gothfox/Tiny-Tiny-RSS.git /opt/Tiny-Tiny-RSS
+
+WORKDIR /opt/Tiny-Tiny-RSS
+
+RUN git checkout ${TTRSS_VERSION_TAG}
+
+RUN apt-get remove --purge -y git-core \
+	&& apt-get autoremove -y --purge \
+	&& apt-get clean
+
+RUN echo "daemon off;" >> /etc/nginx/nginx.conf
+COPY nginx-default /etc/nginx/sites-available/default
+
+RUN touch /var/run/php5-fpm.sock && chown www-data /var/run/php5-fpm.sock
+
+RUN chown -R www-data cache feed-icons lock
+RUN cp config.php-dist config.php
+
+COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+
+EXPOSE 80
+
+CMD ["/usr/bin/supervisord"]
---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+: ${POSTGRES_PORT_5432_TCP_ADDR:?"--link to a PostgreSQL container is not set"}
+: ${SELF_URL_PATH:?"-e SELF_URL_PATH is not set"}
+
+sed -ri 's/;opcache.enable=0/opcache.enable=1/;' /etc/php5/fpm/php.ini
+sed -ri 's/;opcache.enable_cli=0/opcache.enable_cli=1/;' /etc/php5/fpm/php.ini
+sed -ri 's/;opcache.memory_consumption=64/opcache.memory_consumption=128/;' /etc/php5/fpm/php.ini
+sed -ri 's/;opcache.max_accelerated_files=2000/opcache.max_accelerated_files=4000/;' /etc/php5/fpm/php.ini
+sed -ri 's/;opcache.revalidate_freq=2/opcache.revalidate_freq=60/;' /etc/php5/fpm/php.ini
+
+sed -ri 's/"localhost"/"'"${POSTGRES_PORT_5432_TCP_ADDR}"'"/;' config.php
+sed -ri 's/"fox"/"tt_rss"/;' config.php
+sed -ri 's/"XXXXXX"/""/;' config.php
+
+sed -ri 's#http://example\.org/tt-rss/#'"${SELF_URL_PATH}"'#;' config.php
+
+if psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -U postgres -lqt | cut -d \| -f 1 | grep tt_rss; then
+	echo 'database `tt_rss` found; skipping schema installation'
+
+else
+	echo 'database `tt_rss` not found; running schema installation'
+
+	createuser -h "$POSTGRES_PORT_5432_TCP_ADDR" -U postgres --no-superuser --no-createrole --no-createdb tt_rss
+	createdb -h "$POSTGRES_PORT_5432_TCP_ADDR" -U postgres -O tt_rss tt_rss
+	psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -U tt_rss -d tt_rss -a -f schema/ttrss_schema_pgsql.sql
+
+fi
+
+echo '===='
+
+exec "$@"
